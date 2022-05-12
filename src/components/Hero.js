e0fd2bda104cfe8edc3013db89c836fa63b9b44a
Jeff Conn <jeff.conn121@gmail.com>
2022-05-11 20:23:05 -0400

    initialize portfolio

---
+++ src/components/Hero.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { VideoBg } from './NavElements'
+import Video from "./videos/Hero.mp4"
+import './Hero.css'
+
+const Hero = () => {
+    return (
+        <div className='hero'>
+            <VideoBg autoPlay loop muted src={Video} type="video/mp4" />
+            <div className='content'>
+                <p>The name's Conn, Jeff Conn</p>
+                <p>I'm a full stack web developer</p>
+                <button href='/' className='button'>View my work</button>
+            </div>
+        </div>
+    )
+}
+
+export default Hero
